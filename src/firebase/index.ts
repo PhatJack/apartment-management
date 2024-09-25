9eb5715643de4be1546c7e49656c61560d1336b1
PhatJack <tienphatng.693@gmail.com>
2024-09-25 13:15:52 +0700

    prevent public leak key firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,14 +8,14 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAW4EBIqL_HW-1cJ4rl_ePY8PPO5wxsE70',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'apartment-management-42b1a.firebaseapp.com',
   projectId: 'apartment-management-42b1a',
   storageBucket: 'apartment-management-42b1a.appspot.com',
   messagingSenderId: '480707102388',
   appId: '1:480707102388:web:ae322461f419870c9a39e1',
   measurementId: 'G-NBCL62BBZE',
-	databaseURL: 'https://apartment-management-42b1a-default-rtdb.firebaseio.com'
+  databaseURL: 'https://apartment-management-42b1a-default-rtdb.firebaseio.com',
 }
 
 // Initialize Firebase
